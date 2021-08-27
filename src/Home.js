5f9f64cae82146dc76111be4134288331a0e63e2
victorgbaye <victorgbayedev@gmail.com>
2021-08-27 15:40:29 -0700

    added tech stack and built for desktop screen

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,13 @@
 import React from 'react'
 
 import arrow from './down-arrow-svgrepo-com.svg'
+import react from './react-logo-svgrepo-com (1).svg'
+import javascript from './javascript-logo-svgrepo-com (1).svg';
+import css from './css-3-logo-svgrepo-com.svg';
+import html from './html5-svgrepo-com (1).svg';
+import git from './git-icon-logo-svgrepo-com.svg';
+import github from './github-svgrepo-com (1).svg';
+import wave from './waving-hand.png'
 
 function Home() {
     return (
@@ -8,7 +15,7 @@ function Home() {
             <header>
                 <div className="header-main">
                     <div className="header-main-intro">
-                        <h1>Hello, I’m Victor Gbaye.</h1>
+                        <h1>Hello <img src={wave} alt="" className='wave' />, <br/>I’m Victor Gbaye.</h1>
                     </div>
                     <div className="header-main-description">
                         <p>
@@ -22,6 +29,41 @@ function Home() {
                     <img src={arrow} alt="" />
                 </div>
             </header>
+            <main>
+                <a href="https://github.com/victorgbaye"><img src={github} alt="" className="fixed-github"/></a>
+                <div className="tech-stack-container">
+                    <div className="tech-stack-title">
+                        <h2>Tech stack</h2>
+                    </div>
+                    <div className="tech-stack-icon-text-container">
+                        <div className="icon-text">
+                            <img src={react} alt="" />
+                            <p>Reactjs</p>
+                        </div>
+                        <div className="icon-text">
+                            <img src={javascript} alt="" />
+                            <p>Javascript</p>
+                        </div>
+                        <div className="icon-text">
+                            <img src={css} alt="" />
+                            <p>CSS</p>
+                        </div>
+                        <div className="icon-text">
+                            <img src={html} alt="" />
+                            <p>HTML</p>
+                        </div>
+                        <div className="icon-text">
+                            <img src={git} alt="" />
+                            <p>Git</p>
+                        </div>
+                    </div>
+                </div>
+                {/* <div className="contact-container">
+                    <div className="contact-title">
+                        <h2>Contact me</h2>
+                    </div>
+                </div> */}
+            </main>
         </>
     )
 }
